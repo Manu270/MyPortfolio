c6cff139f7ea73d928b80339e77534c8076c13a1
MANOGNA CHITTI <112813299+Manu270@users.noreply.github.com>
2025-03-01 11:25:53 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
-                <li><Link to="/portfolio">Portfolio</Link></li>
+                <li><Link to="/portfolio">Projects</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
